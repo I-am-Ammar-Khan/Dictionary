cb1131af75f9fb1e7c8509b6e5c13b430794db32
Ammar-Khan <18178606-Ammar-Khan@users.noreply.replit.com>
2025-09-26 08:56:55 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const word_meaning = (word) => {
+    fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + word)
+        .then((response) => response.json())
+        .then((data) => {
+            main_word.innerHTML = ''
+            pronounce.innerHTML = ''
+            meaning.innerHTML = ''
+            noun.innerHTML = ''
+            verb.innerHTML = ''
+            adjective.innerHTML = ''
+            
+            console.log(data)
+            eee = data[0].word
+            main_word.innerHTML = `<p ><i>Word: </i>${eee}</p>`
+            if (data[0].phonetics[1]) {
+                eee = data[0].phonetics[1].text
+                pronounce.innerHTML = `<p ><i>Pronounce: </i>${eee}</p>`
+            }
+            if (data[0].meanings[0].definitions[0].definition) {
+                eee = data[0].phonetic
+                eee = data[0].meanings[0].definitions[0].definition
+                meaning.innerHTML = `<p ><i>Meaning: </i><br>${eee}</p>`
+            }
+            if (data[0].meanings[1].definitions[0].definition) {
+                eee = data[0].meanings[1].definitions[0].definition
+                noun.innerHTML = `<p ><i>Verb: </i><br>${eee}</p>`
+
+            }
+            if (data[0].meanings[2].definitions[0].definition) {
+                eee = data[0].meanings[2].definitions[0].definition
+                verb.innerHTML = `<p ><i>Verb: </i><br>${eee}</p>`
+            }
+            if (data[0].meanings[3].definitions[0].definition) {
+                eee = data[0].meanings[3].definitions[0].definition
+                adjective.innerHTML = `<p><i>Adjective: </i><br>${eee}</p>`
+            }
+
+            // voice.addEventListener('click',(e)=>{
+            //     e.preventDefault()
+            //     var audio = new Audio(`${data[0].phonetics[0].audio}`)
+            //     audio.play()
+            // })
+        });
+}
+submit.addEventListener('click', (e) => {
+    e.preventDefault()
+    word_meaning(word.value)
+    word.value = ''
+})
+
+word_meaning('dictionary')
